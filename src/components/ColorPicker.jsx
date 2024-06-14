7a6f05487e57b536ca895a94b16bf74ab96a992a
DrakBlack69 <tybyers21@gmail.com>
2024-06-14 17:24:46 -0400

    Initial commit

---
+++ src/components/ColorPicker.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { proxy, useSnapshot } from 'valtio';
+import '../App.css';
+import RedirectButton from './RedirectButton';
+
+// Define state object with BaseMtl property initialized to white
+export const state = proxy({
+  current: 'none',
+  base: {
+    BaseMtl: "none" // White color
+  }
+});
+
+// Function to update the BaseMtl value in the snap1 state object
+export const updateBaseMtl = (color) => {
+  const snap1Proxy = proxy(state);
+  snap1Proxy.base.BaseMtl = color;
+};
+
+// ColorPicker component
+const ColorPicker = () => {
+  const snap1 = useSnapshot(state);
+
+  return (
+    <>
+      <div className='container'>
+        <div className='btn' onClick={() => updateBaseMtl("#fc9700")}><a>Burnt Orange</a></div>
+        <div className='btn' onClick={() => updateBaseMtl("#FF0000")}><a>Red</a></div>
+        <div className='btn' onClick={() => updateBaseMtl("#0000FF")}><a>Blue</a></div>
+        <div className='btn' onClick={() => updateBaseMtl("#00FF00")}><a>Green</a></div>
+        <div className='btn' onClick={() => updateBaseMtl("#f2ff00")}><a>Yellow</a></div>
+        <div className='btn' onClick={() => updateBaseMtl("#800080")}><a>Purple</a></div>
+        <div className='btn' onClick={() => updateBaseMtl("#808080")}><a>Grey</a></div>
+        {/* <p>Current button: {snap1.current}</p>
+      <p>Current color: {snap1.base.BaseMtl}</p> */}
+      </div>
+      <div className='btn-space'>
+        <button className='glowing-btn' onClick={() => updateBaseMtl("none")}>Reset To Original Color</button>
+      </div>
+      <div className='buy-now'>
+      <RedirectButton />
+      </div>
+    </>
+  );
+};
+
+export default ColorPicker;
---
+++ src/components/Configurator.jsx
@@ -0,0 +1,97 @@
+import { useCustomization } from '../context/Customization';
+import Select, { components } from 'react-select';
+import '../App.css';
+import ColorPicker from './ColorPicker';
+import TexturePaths from './TexturePaths';
+
+const options = [
+    { value: 1, label: 'Raven', image: '/options/raven.png' },
+    { value: 2, label: 'Absolute Slaughter', image: '/options/slaughter.png' },
+    { value: 3, label: 'Bat Lady', image:'/options/bats.png'},
+    { value: 4, label: 'Brake Line', image: '/options/brakeline.png' },
+    { value: 5, label: 'Danger Inside', image: '/options/danger.png' },
+    { value: 6, label: 'Joker', image: '/options/joker.png' },
+    { value: 7, label: 'Madness', image: '/options/madness.png' },
+    { value: 8, label: 'Desert', image: '/options/desert.png' },
+    { value: 9, label: 'Hot Rods', image: '/options/hotrod.png' },
+    { value: 10, label: 'Mayan', image: '/options/mayan.png' },
+    { value: 11, label: 'Sea Breeze', image: '/options/seabreaze.png' },
+    { value: 12, label: 'Shape Shifter', image: '/options/shapeshifter.png' },
+    { value: 13, label: 'Silk', image: '/options/silk.png' },
+    { value: 14, label: 'Snake Skin', image: '/options/snake.png' },
+    { value: 15, label: 'Solar Flare', image: '/options/flare.png' },
+    { value: 16, label: 'Dominator', image: '/options/dominator.png' },
+    { value: 17, label: 'Top Gun', image: '/options/topgun.png' },
+    { value: 18, label: 'Water Explosion', image: '/options/explosion.png' },
+    { value: 19, label: 'Wild West', image: '/options/wildwest.png' },
+    { value: 20, label: 'Zombie', image: '/options/zombie.png' },
+    { value: 21, label: 'Linearock', image: '/options/linea.png' },
+    { value: 22, label: 'Euclids Theorem', image: '/options/euclid.png' },
+    { value: 23, label: 'Pantone Zebra', image: '/options/pantone.png' },
+    { value: 24, label: 'Heavenly Peace', image: '/options/heavenly.png' },
+    { value: 25, label: 'Liquid Army', image: '/options/army.png' },
+    
+];
+
+const Option = ({ data, ...props }) => (
+    <components.Option {...props}>
+        <img
+            src={data.image}
+            alt={data.label}
+            style={{ width: '20px', marginRight: '10px' }}
+        />
+        {data.label}
+    </components.Option>
+);
+
+
+const Configurator = () => {
+    const { overlay, setOverlay } = useCustomization();
+
+    const handleOverlayChange = (selectedOption) => {
+        setOverlay(selectedOption.value);
+    };
+
+
+    return (
+        <>
+
+            <div className='configurator'>
+                <div className='conic all' />
+                <div className='configurator__section__title'>
+                    Select Vehicle Wrap
+                </div>
+                <div className='selector' >
+                    <Select
+                        value={options.find(option => option.value === overlay)}
+                        options={options}
+                        onChange={handleOverlayChange}
+                        placeholder="Select Material"
+                        components={{ Option }}
+                        isSearchable={false}
+                        getOptionLabel={(option) => option.label}
+                        getOptionValue={(option) => option.value}
+                    />
+                    <div className='picker-color'>
+                        <div className='configurator__section__title'>
+                            Change Vehicle Color
+                        </div>
+                        <ColorPicker />
+                    </div>
+                </div>
+
+
+                <div className='texture-images'>
+                    {TexturePaths.map((v) => {
+                        return (
+                            <img key={v.overlay} src={v.path} id={`overlay${v.overlay}`} />
+                        )
+                    })}
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default Configurator;
